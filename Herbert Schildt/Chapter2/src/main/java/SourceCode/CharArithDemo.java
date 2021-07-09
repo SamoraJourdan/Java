b44a4901f6c8f6d2c7e4fe9bfc6b5bc92e27d87d
Samora Jourdan <samorajourdan@gmail.com>
2021-07-09 11:15:11 +0200

    Chapter9

---
+++ Herbert Schildt/Chapter2/src/main/java/SourceCode/CharArithDemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SourceCode;
+
+// Character variables can be handled like integers.  
+class CharArithDemo { 
+  public static void main(String args[]) { 
+    char ch; 
+ 
+    ch = 'X'; 
+    System.out.println("ch contains " + ch); 
+ 
+    ch++; // increment ch 
+    System.out.println("ch is now " + ch); 
+ 
+    ch = 90; // give ch the value Z 
+    System.out.println("ch is now " + ch); 
+  } 
+}
---
+++ Herbert Schildt/Chapter2/src/main/java/SourceCode/LtoD2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SourceCode;
+
+/**
+ *
+ * @author pc
+ */
+// *** This program will not compile. *** 
+class LtoD2 {    
+  public static void main(String args[]) {    
+    long L; 
+    double D; 
+   
+    D = 100123285.0; 
+    //L = D; // Illegal!!! 
+   
+    //System.out.println("L and D: " + L + " " + D); 
+   
+  }    
+}
+
