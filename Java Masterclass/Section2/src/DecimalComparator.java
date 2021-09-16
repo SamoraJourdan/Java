a4ffde2f9e4625dc4f684ff519c5de905b15441c
Samora Jourdan <samorajourdan@gmail.com>
2021-09-16 11:15:35 +0200

    Completed

---
+++ Java Masterclass/Section2/src/DecimalComparator.java
@@ -0,0 +1,15 @@
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+
+        int num11 = (int)(num1 * 1000);
+        int num22 = (int)(num2 * 1000);
+
+        if(num11 == num22){
+            return true;
+        }
+        return false;
+
+    }
+
+}
+
---
+++ Java Masterclass/Section2/src/TeenNumberChecker.java
@@ -0,0 +1,10 @@
+public class TeenNumberChecker {
+    public static boolean hasTeen(int a, int b, int c){
+        return ((13 <= a && a <= 19)||
+                (13 <= b && b <= 19)||
+                (13 <= c && c <= 19));
+    }
+    public static boolean isTeen(int a){
+        return (13 <= a && a <= 19);
+    }
+}
