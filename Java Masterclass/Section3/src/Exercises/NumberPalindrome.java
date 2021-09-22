1934c0a44b85bd27ab64cf2c2be2406b4a396fb6
Samora Jourdan <samorajourdan@gmail.com>
2021-09-22 09:57:11 +0200

    Completed

--- Java Masterclass/Section3/src/Exercises/NumberPalindrome.java
+++ Java Masterclass/Section3/src/Exercises/NumberPalindrome.java
@@ -1,4 +1,22 @@
 package Exercises;
 
 public class NumberPalindrome {
+    public static boolean isPalindrome(int number){
+        int num = number;
+        int reverse = 0;
+
+        while (num != 0) {
+            int lastDigit = num % 10;
+            reverse *= 10;
+            reverse += lastDigit;
+            num /= 10;
+
+        }
+
+        if(reverse == number){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
