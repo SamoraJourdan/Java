5967539207b1e55b255c18fb3a2bf58b845330db
Samora Jourdan <samorajourdan@gmail.com>
2021-09-22 11:32:09 +0200

    Completed

--- Java Masterclass/Section3/src/Exercises/GreatestCommonDivisor.java
+++ Java Masterclass/Section3/src/Exercises/GreatestCommonDivisor.java
@@ -1,4 +1,25 @@
 package Exercises;
 
 public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int num1,int num2){
+        if (num1 < 10 || num2 < 10){
+            return -1;
+        }
+
+        int div = 0;
+        int big;
+
+        if (num1 > num2){
+            big = num1;
+        }else {
+            big = num2;
+        }
+
+        for (int i = 1; i <= big; i++){
+            if (num1 % i == 0 && num2 % i == 0){
+                div = i;
+            }
+        }
+        return div;
+    }
 }
